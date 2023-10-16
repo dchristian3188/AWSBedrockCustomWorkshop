76a640f3c0557fcc93e9ad23f1a69a78039e46d0
David Christian <chradavi@amazon.com>
2023-10-16 11:22:08 -0700

    updated path

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python3 $SCRIPT_DIR/grantNotebookPermissions.py
+python3 $SCRIPT_DIR/grantNotebookpermissions.py
 pushd ./cdk
 npm install
 cdk bootstrap 
