cd83474c405a78d7434303f8b967c27e317c3340
David Christian <chradavi@amazon.com>
2023-10-16 12:46:48 -0700

    updated permissions script for secrets manager

--- grantNotebookpermissions.py
+++ grantNotebookpermissions.py
@@ -20,3 +20,12 @@ response = iam.attach_role_policy(
     PolicyArn='arn:aws:iam::aws:policy/AmazonEC2FullAccess'
     )
 print(response)
+
+# add the secrets manager read only to the role
+print("Granting secrets manager read only to role")
+response = iam.attach_role_policy(
+    RoleName=role['RoleName'],
+    PolicyArn='arn:aws:iam::aws:policy/SecretsManagerReadWriteInfo'
+    )
+print(response)
+
